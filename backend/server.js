b190c4f9d4b84cf186ae704d7b82ac54d765eb09
jordan7510 <cgupta7510@gmail.com>
2024-01-15 19:50:22 +0530

    updated multiple cors

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 // app.use(cors())
-app.use(cors({ credentials: true, origin: 'https://velvet-vista-client.vercel.app' }));
+const allowedOrigins = ['https://velvet-vista-client.vercel.app', 'http://localhost:5173'];
+
+app.use(cors({ credentials: true, origin: allowedOrigins }));
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
